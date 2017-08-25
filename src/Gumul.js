a2668eed26508cd721f31731880f6487348318dd
tester <tester@cheoeum.in>
2017-08-25 09:08:01 +0900

    ing...

--- src/Gumul.js
+++ src/Gumul.js
@@ -4,43 +4,151 @@ import injectTapEventPlugin from 'react-tap-event-plugin'
 import MoreHoriz from 'material-ui-icons/MoreHoriz'
 import IconButton from 'material-ui/IconButton'
 import {withStyles} from 'material-ui/styles'
-import Header from './components/Header'
 
-const Gumul = ({classes, id, title, head, body}) => (
-	<table id={id} className={classes.table}>
-		<caption>
-			{title}
-			<IconButton><MoreHoriz/></IconButton>
-		</caption>
-		<Header html={head}/>
-		{body}
-	</table>
-)
+// material-ui required
+injectTapEventPlugin()
+
+const Gumul = ({classes, id, title, head}) => {
+	const headerCells = generateFromHtml(head)
+
+	console.log(headerCells)
+
+	return (
+		<div id={id} className={classes.base}>
+			<table>
+				<caption>
+					{title}
+					<IconButton><MoreHoriz/></IconButton>
+				</caption>
+			</table>
+		</div>
+	)
+}
 
 Gumul.propTypes = {
 	id: PropTypes.string.isRequired
 }
 
-export default withStyles({
-	'table': {
-		borderCollapse: 'collapse',
-		tableLayout:    'fixed',
-		'& caption':    {
-			whiteSpace: 'nowrap'
-		},
-		'& th>a':       {
-			bottom:     0,
-			display:    'block',
-			height:     '100%',
-			position:   'absolute',
-			right:      -3,
-			width:      7,
-			'&::after': {
-				content: 'xx'
+const generateFromHtml = html => {
+	const cell = {
+		cell: []
+	}
+
+	let id = 0
+	let rowIndex = -1
+	html.props.children.forEach(tr => {
+		rowIndex++
+
+		if (!cell[rowIndex]) {
+			cell[rowIndex] = []
+		}
+
+		let cellIndex = -1
+		tr.props.children.forEach(th => {
+			id++
+			cellIndex++
+
+			if (th.props.colSpan > 1 && th.props.rowSpan > 1) {
+				for (let i = 0; i < th.props.rowSpan; i++) {
+					if (!cell[rowIndex + i]) {
+						cell[rowIndex + i] = []
+					}
+
+					for (let j = 0; j < th.props.colSpan; j++) {
+						while (cell[rowIndex + i][cellIndex + j]) {
+							cellIndex++
+						}
+
+						cell[rowIndex + i][cellIndex + j] = {
+							text: th.props.children,
+							id
+						}
+					}
+				}
+
+				cellIndex += th.props.colSpan - 1
+			}
+			else if (th.props.colSpan > 1) {
+				for (let i = 0; i < th.props.colSpan; i++) {
+					while (cell[rowIndex][cellIndex + i]) {
+						cellIndex++
+					}
+
+					cell[rowIndex][cellIndex + i] = {
+						text: th.props.children,
+						id
+					}
+				}
+
+				cellIndex += th.props.colSpan - 1
+			}
+			else if (th.props.rowSpan > 1) {
+				for (let i = 0; i < th.props.rowSpan; i++) {
+					if (!cell[rowIndex + i]) {
+						cell[rowIndex + i] = []
+					}
+
+					while (cell[rowIndex + i][cellIndex]) {
+						cellIndex++
+					}
+
+					cell[rowIndex + i][cellIndex] = {
+						text: th.props.children,
+						id
+					}
+				}
+			}
+			else {
+				if (!cell[rowIndex]) {
+					cell[rowIndex] = []
+				}
+
+				while (cell[rowIndex][cellIndex]) {
+					cellIndex++
+				}
+
+				cell[rowIndex][cellIndex] = {
+					text: th.props.children,
+					id
+				}
+			}
+		})
+	})
+
+	return cell
+}
+
+export default withStyles(theme => ({
+	'base': {
+		'color':       theme.palette.text.primary,
+		'font-family': ['Roboto', 'NanumSquare', 'sans-serif'],
+		'font-weight': 300,
+		'& table':     {
+			'border-collapse': 'collapse',
+			'table-layout':    'fixed',
+			'& caption':       {
+				'height':      '42px',
+				'font-size':   '1.6rem',
+				'font-weight': 300,
+				'line-height': 42,
+				'padding':     '0 1rem',
+				'text-align':  'left',
+				'& >button':   {
+					'vertical-align': 'middle'
+				}
+			},
+			'& th,td':         {
+				'border-color':  theme.palette.grey.A100,
+				'border-style':  'solid',
+				'border-width':  '1px 0',
+				'font-weight':   300,
+				'height':        27,
+				'overflow':      'hidden',
+				'padding':       '0 6px',
+				'text-overflow': 'ellipsis',
+				'white-space':   'nowrap',
+				'width':         30
 			}
 		}
 	}
-})(Gumul)
-
-// material-ui required
-injectTapEventPlugin()
+}))(Gumul)
--- src/components/Header.js
+++ src/components/Header.js
@@ -10,118 +10,28 @@ export default class Header extends React.Component {
 		console.log('shouldComponentUpdate')
 	}
 
-
 	componentWillMount() {
-		const {html} = this.props
-		const {cell} = this.state
-
-		let rowIndex = -1
-		html.props.children.forEach(tr => {
-			rowIndex++
-
-			if (!cell[rowIndex]) {
-				cell[rowIndex] = []
-			}
-
-			let cellIndex = -1
-			tr.props.children.forEach(th => {
-				cellIndex++
-
-				if (th.props.colSpan > 1 && th.props.rowSpan > 1) {
-					for (let i = 0; i < th.props.rowSpan; i++) {
-						if (!cell[rowIndex + i]) {
-							cell[rowIndex + i] = []
-						}
-
-						for (let j = 0; j < th.props.colSpan; j++) {
-							while (cell[rowIndex + i][cellIndex + j]) {
-								cellIndex++
-							}
-
-							cell[rowIndex + i][cellIndex + j] = {
-								element:    th,
-								duplicated: i > 0 || j > 0
-							}
-						}
-					}
-
-					cellIndex += th.props.colSpan - 1;
-				}
-				else if (th.props.colSpan > 1) {
-					for (let i = 0; i < th.props.colSpan; i++) {
-						while (cell[rowIndex][cellIndex + i]) {
-							cellIndex++
-						}
-
-						cell[rowIndex][cellIndex + i] = {
-							element:    th,
-							duplicated: i > 0
-						}
-					}
-
-					cellIndex += th.props.colSpan - 1;
-				}
-				else if (th.props.rowSpan > 1) {
-					for (let i = 0; i < th.props.rowSpan; i++) {
-						if (!cell[rowIndex + i]) {
-							cell[rowIndex + i] = []
-						}
-
-						while (cell[rowIndex + i][cellIndex]) {
-							cellIndex++
-						}
-
-						cell[rowIndex + i][cellIndex] = {
-							element:    th,
-							duplicated: i > 0
-						}
-					}
-				}
-				else {
-					if (!cell[rowIndex]) {
-						cell[rowIndex] = []
-					}
-
-					while (cell[rowIndex][cellIndex]) {
-						cellIndex++
-					}
-
-					cell[rowIndex][cellIndex] = {
-						element:    th,
-						duplicated: false
-					}
-				}
-			})
-		})
 	}
 
 	render() {
-		const cell = [...this.state.cell]
+		const cells = [...this.state.cell]
 
-		cell.forEach(i => {
-			i[1].duplicated = true
-		})
-
-		console.log(cell)
+		console.log(cells)
 
 		return (
 			<thead>
-			{
+			{/*{
 				cell.map((tr, i) => <tr key={i}>
 					{
 						tr.map((th, j) => this.renderCell(th, j))
 					}
 				</tr>)
-			}
+			}*/}
 			</thead>
 		)
 	}
 
-	renderCell(cell, index) {
-		if (!cell.duplicated) {
-			return (
-				<th key={index} {...cell.element.props}></th>
-			)
-		}
+	renderCell(cells, ) {
+
 	}
 }
